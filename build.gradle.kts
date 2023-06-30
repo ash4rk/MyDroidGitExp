7180216e81e157c092b00cfbb6239e0deaaa48d1
ash4rk <cool.shark.98@mail.ru>
2023-06-30 07:34:09 +0300

    FIX: Remove spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-plugins {
-    id("com.diffplug.spotless") version "6.7.2"
-}
 buildscript {
 
     repositories {
@@ -16,41 +13,13 @@ buildscript {
     }
 }
 
-
 subprojects {
-    apply(plugin = "com.diffplug.spotless")
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
         kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
         kotlinOptions.freeCompilerArgs += listOf(
-            "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            "-Xopt-in=kotlin.time.ExperimentalTime",
+            "-X opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-X opt-in=kotlin.time.ExperimentalTime",
         )
-    }
-
-    extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        kotlin {
-            target("**/*.kt")
-            targetExclude("$buildDir/**/*.kt")
-            ktlint().setUseExperimental(true).editorConfigOverride(
-                mapOf(
-                    "indent_size" to "2",
-                    "continuation_indent_size" to "2"
-                )
-            )
-            licenseHeaderFile(rootProject.file("spotless/spotless.license.kt"))
-            trimTrailingWhitespace()
-            endWithNewline()
-        }
-        format("kts") {
-            target("**/*.kts")
-            targetExclude("$buildDir/**/*.kts")
-            licenseHeaderFile(rootProject.file("spotless/spotless.license.kt"), "(^(?![\\/ ]\\*).*$)")
-        }
-        format("xml") {
-            target("**/*.xml")
-            targetExclude("**/build/**/*.xml")
-            licenseHeaderFile(rootProject.file("spotless/spotless.license.xml"), "(<[^!?])")
-        }
     }
 }
