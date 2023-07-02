d4c871d2ac34d89bb155db273aa0419a278fe95c
ash4rk <cool.shark.98@mail.ru>
2023-07-02 06:34:46 +0300

    FEAT: Introduce search toolbar button

--- app/src/main/java/com/example/mydroidgitexp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mydroidgitexp/ui/main/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.mydroidgitexp.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.annotation.VisibleForTesting
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.mydroidgitexp.R
 import com.example.mydroidgitexp.adapters.UserAdapter
 import com.example.mydroidgitexp.databinding.ActivityMainBinding
 import com.skydoves.bindables.BindingActivity
 
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Qualifier
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
@@ -22,5 +31,23 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             adapter = UserAdapter()
             vm = viewModel
         }
+
+        binding.toolbar.run {
+            inflateMenu(R.menu.main_menu)
+            setOnMenuItemClickListener { item ->
+                if (item.itemId == R.id.search) {
+                    Timber.d("Navigate to Search")
+                    openSearch()
+                    true
+                } else {
+                    false
+                }
+            }
+        }
     }
+}
+
+private fun openSearch() {
+    // TODO: navigate via NavController
+    return
 }
